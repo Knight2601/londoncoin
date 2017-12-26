9ddc6742ac9f06db60b8efca2c8d31a1a3a6b017
Ubuntu <ubuntu@ip-172-31-89-47.ec2.internal>
2017-12-26 19:04:40 +0000

    londoncoin initial push

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/crypto/cryptonote/src
+# Build directory: /home/ubuntu/crypto/cryptonote/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
